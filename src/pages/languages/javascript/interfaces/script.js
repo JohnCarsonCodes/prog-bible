350b3af6d9e797c2d27f945356ad3a9043f13420
John Carson <johncarsoncodes@gmail.com>
2024-05-29 19:11:59 -0400

    Updates

--- src/pages/languages/javascript/interfaces/script.js
+++ src/pages/languages/javascript/interfaces/script.js
@@ -7,6 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+    fetch('/src/pages/languages/javascript/interfaces/events-sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar-events-placeholder').innerHTML = data;
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     fetch('/src/pages/languages/javascript/interfaces/crumbs.html')
         .then(response => response.text())
         .then(data => {

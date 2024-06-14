421863bea2da6b3d41adcdd1435a88e029582839
John Carson <johncarsoncodes@gmail.com>
2024-06-14 14:48:51 -0400

    Deleted Script.js from multiple folders

--- src/pages/languages/javascript/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('/src/pages/languages/javascript/crumbs.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('crumbs-placeholder').innerHTML = data;
-        });
-});
